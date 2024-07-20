015ae0438d85fcdd138fbf65621bb26347ace08b
saniya <saniyaaaaakk@gmail.con>
2024-07-20 10:25:20 +0530

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by SANIYA KHATARKAR on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
